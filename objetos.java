0ed3d469ed9f1ede4698aa37019cac4b403993f3
OScarSanchez-lab <oscar.sanchez@gracia.lasalle.cat>
2026-01-30 13:21:28 +0100

    feat: atributos de los personajes

---
+++ objetos.java
@@ -0,0 +1,5 @@
+package videojuego-clases;
+
+public class objetos {
+    
+}
---
+++ personajes.java
@@ -0,0 +1,10 @@
+package videojuego-clases;
+
+public class personajes {
+    private String nombre;
+    private int salud;
+    private int fuerza;
+    private int nivel;
+
+    
+}
